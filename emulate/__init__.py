51acb17549e2f515bbd98584de452a61c5d62367
Alberto J Garcia <albertojg1991@gmail.com>
2023-05-19 08:40:18 -0400

    Update __init__.py

--- emulate/__init__.py
+++ emulate/__init__.py
@@ -3,4 +3,5 @@ from .mesh import BuildMesh
 from .emulator_scattering import KVP_emulator_scattering
 from .emulator_bound import KVP_emulator_bound
 from .square_well import SquareWell
+from .plots import setup_rc_params, plot_scattering_wf, plot_bound_results
 
